4198450b39b4a4c37a3dcfa09e8e33ae4da7efe3
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-14 04:42:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,8 @@ second_number = 1
 
 sum = first_number + second_number
 
+difference = second_number - first_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
